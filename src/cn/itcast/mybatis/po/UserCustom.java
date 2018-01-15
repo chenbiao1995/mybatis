1743c345affa27b7b8b6fd5ebec4d5b7e78dbd89
lenovo <lenovo@DESKTOP-IQCPEHJ>
2018-01-15 17:00:46 +0800

    mapper动态代理方法和动态sql语句

---
+++ src/cn/itcast/mybatis/po/UserCustom.java
@@ -0,0 +1,6 @@
+package cn.itcast.mybatis.po;
+//用户的拓展类
+public class UserCustom extends User {
+	//拓展用户的信息
+	
+}
--- test/cn/itcast/mybatis/mapper/UserMapperTest.java
+++ test/cn/itcast/mybatis/mapper/UserMapperTest.java
@@ -3,6 +3,7 @@ package cn.itcast.mybatis.mapper;
 import static org.junit.Assert.*;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
@@ -13,6 +14,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import cn.itcast.mybatis.po.User;
+import cn.itcast.mybatis.po.UserCustom;
+import cn.itcast.mybatis.po.UserQueryVo;
 
 public class UserMapperTest {
 	private SqlSessionFactory sqlSessionFactory;
@@ -46,6 +49,61 @@ public class UserMapperTest {
 	//调用userMapper的方法
 		List<User> list = userMapper.findUserByName("小明");
 		System.out.println(list);
+		
+	}
+	
+	//用户信息的综合查询
+	@Test
+	public void testFindUserList() throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+	//创建UserMapper的对象，mybatis自动生成mapper的代理对象
+		UserMapper userMapper =sqlSession.getMapper(UserMapper.class);
+	//创建包装对象，设置查询条件
+		UserQueryVo userQueryVo = new UserQueryVo();
+		UserCustom userCustom =new UserCustom();
+		//由于这里设置动态sql，如果不设置某个值，条件不会拼接在sql中
+//		userCustom.setSex("1");
+		userCustom.setUsername("小明");
+		//传入多个id
+		List<Integer> ids = new ArrayList<Integer>();
+		ids.add(1);
+		ids.add(10);
+		ids.add(16);
+		//将ids传入statement中
+		userQueryVo.setIds(ids);
+		userQueryVo.setUserCustom(userCustom);
+	//调用userMapper的方法
+		List<UserCustom> list = userMapper.findUserList(userQueryVo);
+		System.out.println(list);
+		
+	}
+	
+	@Test
+	public void testFindUserCount() throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+	//创建UserMapper的对象，mybatis自动生成mapper的代理对象
+		UserMapper userMapper =sqlSession.getMapper(UserMapper.class);
+	//创建包装对象，设置查询条件
+		UserQueryVo userQueryVo = new UserQueryVo();
+		UserCustom userCustom =new UserCustom();
+		userCustom.setSex("1");
+		userCustom.setUsername("陈小明");
+		userQueryVo.setUserCustom(userCustom);
+	//调用userMapper的方法
+		int count = userMapper.findUserCount(userQueryVo);
+		System.out.println(count);
+		
+	}
+	
+	
+	@Test
+	public void testFindUserByIdResultMap() throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+	//创建UserMapper的对象，mybatis自动生成mapper的代理对象
+		UserMapper userMapper =sqlSession.getMapper(UserMapper.class);
+	//调用userMapper的方法
+		User user = userMapper.findUserByIdResultMap(1);
+		System.out.println(user);
 		
 	}
 }
